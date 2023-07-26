d51afe93a75fcf9373278d723fa7fce1e74440a1
duotaro <taro8815@gmail.com>
2023-07-26 19:13:24 +0900

    cloudflare

--- src/lib/cloudflare.ts
+++ src/lib/cloudflare.ts
@@ -1,13 +1,15 @@
 import ENV from "@/utils/env"
-import { PNG, MP4 } from "@/utils/const"
+import { 
+    PNG, MP4, 
+    CATEGORY_MISAKI,CATEGORY_OTHER,CATEGORY_YOTA 
+} from "@/utils/const"
 
 export class CloudFlareFile {
-    path: string = ''
-    type: string = ''
+  path: string = ''
+  type?: string = ''
+  category?: string[] = ['other']
 
-
-
-  constructor(path: string) {
+  constructor(path: string, category?:string[]) {
     this.path = path;
     if(path.indexOf(`.png`) > -1){
         this.type = PNG
@@ -16,16 +18,23 @@ export class CloudFlareFile {
     } else {
         this.type = ''
     }
+    this.category = category
   }
 }
 
-export const fileNameList:string[] = [
-    "video-99970fd8d3bc95c64c437e243e195c71.mp4",
-    "screen.png"
+/**
+ * ファイル名を定義します
+ */
+export const fileNameList:CloudFlareFile[] = [
+    {path:"video-99970fd8d3bc95c64c437e243e195c71.mp4", type:'', category:[CATEGORY_MISAKI]},
+    {path:"screen.png", type:'',category:[CATEGORY_YOTA]}
 ]
 
-export const fileList:CloudFlareFile[] = fileNameList.map((fileName) => {
-    return new CloudFlareFile(`${ENV.CLOUD_FLARE_PUBLIC_URL}${fileName}`)
+/**
+ * ファイル名から描画に必要な属性を判定します
+ */
+export const fileList:CloudFlareFile[] = fileNameList.map((file:CloudFlareFile) => {
+    return new CloudFlareFile(`${ENV.CLOUD_FLARE_PUBLIC_URL}${file.path}`, file.category)
 })
 
 
--- src/utils/const.ts
+++ src/utils/const.ts
@@ -4,3 +4,12 @@ export const MP4:string = 'video/mp4'
 export const FILE_TYPE:string[] = [
     PNG,MP4
 ]
+
+export const CATEGORY_MISAKI:string = 'misaki'
+export const CATEGORY_YOTA:string = 'yota'
+export const CATEGORY_OTHER:string = 'other'
+export const FILE_CATEGORY:string[] = [
+    CATEGORY_MISAKI,
+    CATEGORY_YOTA,
+    CATEGORY_OTHER
+]
